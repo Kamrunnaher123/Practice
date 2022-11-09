1a769e2bcf3301720523d5f50d2e518ee715a5dc
Kamrunnaher123 <117587784+Kamrunnaher123@users.noreply.github.com>
2022-11-09 18:56:36 +0600

    Add files via upload

---
+++ ArrayAddition.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+public class ArrayAddition {
+    public static void main(String args[]) {
+        int[] even = { 2, 4, 6 };
+        int[] odd = { 1, 3, 5 };
+        int[] result = add(even, odd);
+        System.out.println("first array: " + Arrays.toString(even));
+        System.out.println("second array: " + Arrays.toString(odd));
+        System.out.println("sum of array: " + Arrays.toString(result));
+        int[] prime = { 2, 3, 5, 7 };
+        result = add(even, prime);
+        System.out.println("first array: " + Arrays.toString(even));
+        System.out.println("second array: " + Arrays.toString(prime));
+        System.out.println("sum of array: " + Arrays.toString(result));
+    }
+    public static int[] add(int[] first, int[] second) {
+        int length = first.length < second.length ? first.length
+                : second.length;
+        int[] result = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            result[i] = first[i] + second[i];
+        }
+
+        return result;
+    }
+
+}
