cbbae227214e31063258ccfd96a8b257dc24756e
Kamrun Naher <117587784+Kamrunnaher123@users.noreply.github.com>
2022-11-11 11:28:46 +0600

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,37 @@
+
+public class Account{
+    private int account_number;
+    private double account_balance;
+
+    Account()
+    {
+        account_number=0;
+        account_balance=0;
+    }
+
+    Account(int account_number,double account_balance)
+    {
+        this.account_balance=account_balance;
+        this.account_number=account_number;
+    }
+
+    public void setInput(int account_number,Double account_balance){
+        this.account_balance=account_balance;
+        this.account_number=account_number;
+    }
+
+    public void show_data(){
+    System.out.println("Account Number :"+account_number);
+    System.out.println("Account Balance :"+account_balance);
+    }
+
+    public void deposit(double update_value){
+        if (update_value<0){
+            System.out.println("Negetive value shouldn't deposit");
+        }
+        else 
+        account_balance = account_balance+update_value;
+
+    }
+
+}
--- Driver.java
+++ Driver.java
@@ -1,7 +1,17 @@
 public class Driver {
     public static void main(String args[]){
-        Rectangular rect=new Rectangular();
+        /*  Rectangular rect=new Rectangular();
+        Circle circle=new Circle();
         System.out.println("Area of Rectangular : "+rect.compute(52.2f, 40.0f));
+        System.out.println("Area of Circle :"+Circle.compute(10.5f,63.0f));
+        */
+
+        Account account=new Account();
+        account.show_data();
+        account.setInput(101, 10502.65);
+        account.show_data();
+        account.deposit(500.5);
+        account.show_data();
         
     }
 }
